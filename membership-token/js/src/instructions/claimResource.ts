797fffc052e1e319737490a471d087afb5430758
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2022-02-03 11:04:33 +0200

    feat: add withdraw and claim

--- membership-token/js/src/instructions/claimResource.ts
+++ membership-token/js/src/instructions/claimResource.ts
@@ -1,107 +1,93 @@
-import * as web3 from '@solana/web3.js';
-import * as beet from '@metaplex-foundation/beet';
 import * as splToken from '@solana/spl-token';
+import * as beet from '@metaplex-foundation/beet';
+import * as web3 from '@solana/web3.js';
 
 import { PROGRAM_ID } from '../consts';
 
-export type InitSellingResourceInstructionArgs = {
-  masterEditionBump: number;
+export type ClaimResourceInstructionArgs = {
   vaultOwnerBump: number;
-  maxSupply: beet.COption<beet.bignum>;
 };
-const initSellingResourceStruct = new beet.FixableBeetArgsStruct<
-  InitSellingResourceInstructionArgs & {
+const claimResourceStruct = new beet.BeetArgsStruct<
+  ClaimResourceInstructionArgs & {
     instructionDiscriminator: number[];
   }
 >(
   [
     ['instructionDiscriminator', beet.uniformFixedSizeArray(beet.u8, 8)],
-    ['masterEditionBump', beet.u8],
     ['vaultOwnerBump', beet.u8],
-    ['maxSupply', beet.coption(beet.u64)],
   ],
-  'InitSellingResourceInstructionArgs',
+  'ClaimResourceInstructionArgs',
 );
-export type InitSellingResourceInstructionAccounts = {
-  store: web3.PublicKey;
-  admin: web3.PublicKey;
+export type ClaimResourceInstructionAccounts = {
+  market: web3.PublicKey;
+  treasuryHolder: web3.PublicKey;
   sellingResource: web3.PublicKey;
   sellingResourceOwner: web3.PublicKey;
-  resourceMint: web3.PublicKey;
-  masterEdition: web3.PublicKey;
-  metadata: web3.PublicKey;
   vault: web3.PublicKey;
+  metadata: web3.PublicKey;
   owner: web3.PublicKey;
-  resourceToken: web3.PublicKey;
+  destination: web3.PublicKey;
+  clock: web3.PublicKey;
+  tokenMetadataProgram: web3.PublicKey;
 };
 
-const initSellingResourceInstructionDiscriminator = [56, 15, 222, 211, 147, 205, 4, 145];
+const claimResourceInstructionDiscriminator = [0, 160, 164, 96, 237, 118, 74, 27];
 
 /**
- * Creates a _InitSellingResource_ instruction.
+ * Creates a _ClaimResource_ instruction.
  *
  * @param accounts that will be accessed while the instruction is processed
  * @param args to provide as instruction data to the program
  */
-export function createInitSellingResourceInstruction(
-  accounts: InitSellingResourceInstructionAccounts,
-  args: InitSellingResourceInstructionArgs,
+export function createClaimResourceInstruction(
+  accounts: ClaimResourceInstructionAccounts,
+  args: ClaimResourceInstructionArgs,
 ) {
   const {
-    store,
-    admin,
+    market,
+    treasuryHolder,
     sellingResource,
     sellingResourceOwner,
-    resourceMint,
-    masterEdition,
-    metadata,
     vault,
+    metadata,
     owner,
-    resourceToken,
+    destination,
+    clock,
+    tokenMetadataProgram,
   } = accounts;
 
-  const [data] = initSellingResourceStruct.serialize({
-    instructionDiscriminator: initSellingResourceInstructionDiscriminator,
+  const [data] = claimResourceStruct.serialize({
+    instructionDiscriminator: claimResourceInstructionDiscriminator,
     ...args,
   });
   const keys: web3.AccountMeta[] = [
     {
-      pubkey: store,
+      pubkey: market,
       isWritable: false,
       isSigner: false,
     },
     {
-      pubkey: admin,
-      isWritable: true,
-      isSigner: true,
+      pubkey: treasuryHolder,
+      isWritable: false,
+      isSigner: false,
     },
     {
       pubkey: sellingResource,
-      isWritable: true,
-      isSigner: true,
+      isWritable: false,
+      isSigner: false,
     },
     {
       pubkey: sellingResourceOwner,
       isWritable: false,
-      isSigner: false,
+      isSigner: true,
     },
     {
-      pubkey: resourceMint,
-      isWritable: false,
-      isSigner: false,
-    },
-    {
-      pubkey: masterEdition,
-      isWritable: false,
+      pubkey: vault,
+      isWritable: true,
       isSigner: false,
     },
     {
       pubkey: metadata,
-      isWritable: false,
-      isSigner: false,
-    },
-    {
-      pubkey: vault,
       isWritable: true,
       isSigner: false,
     },
@@ -111,17 +97,22 @@ export function createInitSellingResourceInstruction(
       isSigner: false,
     },
     {
-      pubkey: resourceToken,
+      pubkey: destination,
       isWritable: true,
       isSigner: false,
     },
     {
-      pubkey: web3.SYSVAR_RENT_PUBKEY,
+      pubkey: clock,
       isWritable: false,
       isSigner: false,
     },
     {
       pubkey: splToken.TOKEN_PROGRAM_ID,
+      isWritable: false,
+      isSigner: false,
+    },
+    {
+      pubkey: tokenMetadataProgram,
       isWritable: false,
       isSigner: false,
     },
